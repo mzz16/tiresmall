7483f4f8fadbb69d6a35c5d39dd17f104bf01526
MinSeok <dlarkddl@gmail.com>
2023-02-22 12:30:34 +0900

    일단 중간점검

---
+++ src/main/webapp/resources/js/admin/admin.js
@@ -0,0 +1,39 @@
+$(function() {
+	let sub = $('#whatMenu').val();
+	if(sub == 'order'){
+		subMenu()
+		$("#subMenu").css("display","none");
+		$("#adminContent").css("top","90px");		
+	}else if(sub == 'tire'){
+		subMenu()
+		$("#subMenu").css("display","fiex");
+		$("#adminContent").css("top","140px");
+	}else if(sub == 'car'){
+		subMenu()
+		$("#subMenu").css("display","fiex");
+		$("#adminContent").css("top","140px");
+	}else if(sub == 'auth'){
+		subMenu()
+		$("#subMenu").css("display","none");
+		$("#adminContent").css("top","90px");
+	}else if(sub == 'store'){
+		subMenu()
+		$("#subMenu").css("display","none");
+		$("#adminContent").css("top","90px");
+	}else if(sub == 'notice'){
+		subMenu()
+		$("#subMenu").css("display","fiex");
+		$("#adminContent").css("top","140px");
+	}
+});
+
+
+function subMenu() {
+	$("#menuTitle").css("border-top-left-radius","20px");
+	$("#menuTitle").css("border-top-right-radius","20px");
+	$("#menuTitle").css("border-color","white");
+	
+	
+}
+
+
