0b06f2c80f06b5ca8ce2d75c67856b992dcd77f2
MinSeok <dlarkddl@gmail.com>
2023-03-02 17:59:52 +0900

    민석

--- src/main/java/com/tireshoppingmall/home/admin/auth/AuthDAO.java
+++ src/main/java/com/tireshoppingmall/home/admin/auth/AuthDAO.java
@@ -18,4 +18,35 @@ public class AuthDAO {
 		
 	}
 
+
+	public void searchAuth(HttpServletRequest req) {
+		
+		
+	}
+
+
+	public void searchAuth(HttpServletRequest req, AuthDTO aDTO) {
+		//아이디, 이름, 자동차번호
+		req.setAttribute("manyAuth",ss.getMapper(AdminAuthMapper.class).seachAuth(aDTO));
+		
+		
+		
+	}
+
+
+	public void authDelete(HttpServletRequest req, AuthDTO aDTO) {
+		if(ss.getMapper(AdminAuthMapper.class).authDelete(aDTO)==1) {
+			req.setAttribute("r", "삭제 성공");
+		}
+		
+	}
+
+
+	public void updateAuth(HttpServletRequest req, AuthDTO aDTO) {
+		if(ss.getMapper(AdminAuthMapper.class).authUpdate(aDTO)==1) {
+			req.setAttribute("r", "삭제 성공");
+		}
+		
+	}
+
 }
--- src/main/resources/minseok_auth.sql
+++ src/main/resources/minseok_auth.sql
@@ -13,16 +13,34 @@ create table my_car(
     mc_id varchar2(50 char )not null,
     mc_model varchar2(50 char )not null,
     mc_brand varchar2(50 char )not null,
-    mc_year number(4)not null
+    mc_year number(4)not null,
+    
+    FOREIGN KEY(mc_id)
+    REFERENCES auth(a_id) ON DELETE CASCADE
 );
 
 insert into auth  values('mz1004','mz1004','관리자','mz','01012341234','서울',sysdate);
 insert into auth  values('ms1004','ms1004','관리자','민석','01012341234','충청남도',sysdate);
-insert into auth  values('yw1004','yw1004','회원','영웅','01012341234','대구',sysdate);
-insert into auth  values('jh1004','jh1004','회원','재현','01012341234','인천',sysdate);
-insert into auth  values('hg1004','hg1004','회원','한길','01012341234','서울',sysdate);
-insert into auth  values('gn1004','gn1004','회원','건우','01012341234','부산',sysdate);
+insert into auth  values('yw1004','yw1004','일반','영웅','01012341234','대구',sysdate);
+insert into auth  values('jh1004','jh1004','일반','재현','01012341234','인천',sysdate);
+insert into auth  values('hg1004','hg1004','일반','한길','01012341234','서울',sysdate);
+insert into auth  values('gn1004','gn1004','일반','건우','01012341234','부산',sysdate);
+insert into auth  values('gn1004','gn1004','일반','건우','01012341234','부산',sysdate);
+insert into auth  values('1','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('2','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('3','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('4','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('5','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('6','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('7','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('8','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('9','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('10','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('11','ms1004','일반','민석','01012341234','충청남도',sysdate);
+insert into auth  values('12','ms1004','일반','민석','01012341234','충청남도',sysdate);
 
-select * from auth;
 
+select * from auth ;
+delete auth;
 drop table auth;
+drop table my_car;
