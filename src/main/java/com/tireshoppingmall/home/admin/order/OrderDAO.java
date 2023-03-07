4158e221e3afaf28cb21fce2e143507034ee9d57
(WoongMom) <dgyo1209@gmail.com>
2023-03-07 18:03:33 +0900

    3.7 작업

---
+++ src/main/java/com/tireshoppingmall/home/admin/order/OrderDAO.java
@@ -0,0 +1,11 @@
+package com.tireshoppingmall.home.admin.order;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderDAO {
+
+	
+	
+	
+}
--- src/main/resources/Hero.sql
+++ src/main/resources/Hero.sql
@@ -155,3 +155,46 @@ select count(*)
         from(select * from qna q, auth a where q.q_id = a.a_id
         and q.q_title like '%대%' and a.a_id = 'wm2' and a.a_name = '박영웅2' order by q_no))
         where rn >= 1 and rn <= 2;
+--------------------------(order table)-------------------------       
+create table product_order (
+    o_no number(10) primary key,                -- pk
+    o_sortation varchar2(10 char) not null,     -- 회원 / 비회원으로 처리
+    o_ordernumber varchar2(30 char) not null,   -- 주문 번호 결제일시date + UUID로 처리
+    o_orderdate date not null,                  -- 주문 일시
+    o_ordername varchar2(20 char) not null,     -- ex)(2본 / 4본) // (2ea / 4ea)
+    o_product varchar2(100 char) not null,      -- ex) (2본 ! A 타이어 2, 2본 ! B 타이어) 갯수랑 타이어 여러개 구매할때 split으로 자바에서 처리 (장바구니)
+    o_price number(10) not null,                -- 총 가격
+    o_paymethod varchar2(10 char) not null,     -- 지불 방법 무통장/ 현장결제
+    o_deliverymethod varchar2(10 char),         -- 추후 삭제하던지 있던지
+    o_step varchar2(10 char) not null,          -- 결제완료/ 결제대기/결제취소/배송준비중/배송중/배송완료 radio로 처리
+    o_storeshop varchar2(100 char) not null,    -- 장착점 ( 관리자 페이지에선 배송지로 처리)
+    o_tireinstalldate date not null,            -- 장착예정일
+    o_name varchar2(20 char) not null,          -- 구매자 이름
+    o_phone varchar2(15 char) not null,         -- 구매자 전화번호
+    o_email varchar2(30 char),                  -- 구매자 이메일
+    o_caryear varchar2(5 char),                 -- 연식
+    o_carbrand varchar2(15 char),               -- 브랜드
+    o_carname varchar2(30 char),                -- 자동차 이름 ( 위 3가지는 null 가능 없을시 request 에서 기술해달라고 요청)
+    o_carnumber varchar2(10 char) not null,     -- 자동차 번호판 (필수)
+    o_request varchar2(1000 char)               -- 차종을 선택할 수 없는 경우 이곳에 차량정보를 적어주세요!
+);
+
+drop table product_order;
+
+
+insert into product_order values(o_no_seq.nextval, '회원', sysdate, '20230307120000ED484C',     
+    create sequence o_no_seq;
+----------(tire)-----------------------    
+create table tire(
+    t_no number(10) primary key,
+    t_hg varchar2(100 char) not null,
+    t_speed varchar2(50 char) not null,
+    t_code varchar2(50 char),
+    t_brand varchar2(30 char) not null,
+    t_name varchar2(50 char) not null,
+    t_img varchar2(1000 char),
+    t_size varchar2(500 char) not null,
+    t_stock number(5) not null
+)
+
+create sequence t_no_seq;        
