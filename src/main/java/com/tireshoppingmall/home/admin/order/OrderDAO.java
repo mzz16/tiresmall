d24128c0914f3d6aa6ac1d9b9b049edf76b0fa3d
aruken <iamjaehyun4@gmail.com>
2023-03-13 18:15:29 +0900

    Merge remote-tracking branch 'origin/dev' into dev
    # Conflicts:
    #	src/main/java/com/tireshoppingmall/home/admin/car/AdminCarController.java
    #	src/main/java/com/tireshoppingmall/home/admin/order/AdminOrderController.java
    #	src/main/java/com/tireshoppingmall/home/admin/order/OrderDAO.java

--- src/main/java/com/tireshoppingmall/home/admin/order/OrderDAO.java
+++ src/main/java/com/tireshoppingmall/home/admin/order/OrderDAO.java
@@ -1,18 +1,87 @@
 package com.tireshoppingmall.home.admin.order;
 
+import java.math.BigDecimal;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class OrderDAO {
 
+	@Autowired
+	private SqlSession ss;
 	
+	@Autowired
+	private OrderOption oo;
 	
+	private int allOrderCount;
 	
-	
-	
-	
-	
+	public int getAllOrderCount() {
+		return allOrderCount;
+	}
 
+	public void setAllOrderCount(int allOrderCount) {
+		this.allOrderCount = allOrderCount;
+	}
+
+	public void searchOrder(OrderSearchDTO osDTO, HttpServletRequest req) {
+		req.getSession().setAttribute("orderSearchDTO", osDTO);
+	}
+	
+	public void calcAllOrderCount() {
+		OrderSearchDTO ost = new OrderSearchDTO(null, null, "", "", "", "", "");
+		allOrderCount = ss.getMapper(AdminOrderMapper.class).getOrderCount(ost);
+		System.out.println(allOrderCount);
+	}
+	
+	public void getOrder(int pageNo, HttpServletRequest req) {
+		int count = oo.getOrderCountPerPage();
+		int start = (pageNo - 1) * count + 1;
+		int end = start + (count - 1);
+		
+		OrderSearchDTO orderSearch = (OrderSearchDTO) req.getSession().getAttribute("orderSearchDTO");
+		int orderCount = 0;
+		if(orderSearch == null) {
+			orderSearch = new OrderSearchDTO(new BigDecimal(start), new BigDecimal(end), "", "", "", "", "");
+			orderCount = allOrderCount;
+			System.out.println("null이면---" + allOrderCount);
+		}else {
+			orderSearch.setStart(new BigDecimal(start));
+			orderSearch.setEnd(new BigDecimal(end));
+			orderCount = ss.getMapper(AdminOrderMapper.class).getOrderCount(orderSearch);
+		}
+	
+		List<OrderDTO> orders = ss.getMapper(AdminOrderMapper.class).getOrder(orderSearch);
+	
+		int pageCount = (int) Math.ceil(orderCount / (double) count);
+		
+		req.setAttribute("orders", orders);
+		req.setAttribute("pageCount", pageCount);
+		req.setAttribute("curPage", pageNo);
+		
+	}
+
+	public void updateOrder(OrderSearchDTO osDTO, HttpServletRequest req) {
+		if(ss.getMapper(AdminOrderMapper.class).updateOrder(osDTO) >= 1){
+			req.setAttribute("orderUpdate", "업데이트 성공");
+		}else {
+			req.setAttribute("orderUpdate", "업데이트 실패");
+		}
+	}
+
+	public void deleteOrder(OrderSearchDTO osDTO, HttpServletRequest req) {
+		if(ss.getMapper(AdminOrderMapper.class).deleteOrder(osDTO)) {
+			req.setAttribute("orderDelete", "삭제 성공");
+			allOrderCount--;
+			/* reg 할때 allOrderCount++; 해주세용*/
+			}else {
+			req.setAttribute("orderDelete", "삭제 실패");
+		}
+	}
 	
 }
--- src/main/java/com/tireshoppingmall/home/order/CartDAO.java
+++ src/main/java/com/tireshoppingmall/home/order/CartDAO.java
@@ -1,6 +1,6 @@
 package com.tireshoppingmall.home.order;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -9,9 +9,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class CartDAO {
 
-	public int addCart(CartDTO cDTO, HttpServletRequest req) {
-		System.out.println(cDTO.getTg_name());
-		ArrayList<CartDTO> cList = (ArrayList<CartDTO>) req.getSession().getAttribute("cartSession");
+	public int addCart(List<CartDTO> cList, CartDTO cDTO, HttpServletRequest req) {
+		cList = (List<CartDTO>) req.getSession().getAttribute("cartSession");
+		for (CartDTO cart : cList) {
+			if (cart.getTg_id() == cDTO.getTg_id()) {
+				return 0;
+			}
+		}
 		cList.add(cDTO);
 		System.out.println(cList);
 		return 1;
